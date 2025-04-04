1d24b3aa350a1a3c6c1ff27b8088b775676c4add
Yakup Kavak <yakupkavk@gmail.com>
2025-04-04 10:34:03 +0300

    Initial Commit

---
+++ Linkedin/LinkedinApp.swift
@@ -0,0 +1,20 @@
+//
+//  LinkedinApp.swift
+//  Linkedin
+//
+//  Created by Yakup Kavak on 4.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct LinkedinApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
